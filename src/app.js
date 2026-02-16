dec19196a8068973a4c136968aa7fde1945b7d69
Sawan jha <jhasawan02@gmail.com>
2026-02-16 16:44:27 +0530

    succesfully added one user details and class works

--- src/app.js
+++ src/app.js
@@ -1,22 +1,41 @@
 const express = require("express");
 const app = express();
 const connectDB = require("./config/database");
-
-
-
-
-app.get("/",(req,res)=>{
-    res.send("welcome to the home page of dev tinder");
-});
+const User = require("./models/user");
+app.use(express.json());
 
 connectDB()
   .then(() => {
-    console.log("succesfully connected to the dadabase cluster");
+    console.log("successfully connected to the database cluster");
 
     app.listen(8080, () => {
-      console.log(`server is running and  listening from port : 8080`);
+      console.log(`server is running and listening from port : 8080`);
     });
   })
-  .catch(() => {
-    console.log("error while connecting with the database cluster");
+  .catch((err) => {
+    console.log("Database connection error:");
+    console.log(err);
   });
+
+
+
+app.post("/register",async (req, res) => {
+   const user =  new User({
+        firstName:"sachin",
+        lastName: "tendulkar",
+        gender: "male",
+        password: "sachin@123",
+        email: "sachin@tendulkar@gmail.com",
+        age: 55,
+    });
+
+    try{
+    await user.save();
+    res.send({"message":"user registered successfully"});
+    }catch(err){
+        res.status(404).send({"message":"error while registering the user"});
+    }
+})
+
+
+
