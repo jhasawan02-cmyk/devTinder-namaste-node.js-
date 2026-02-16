c9843c3c0bb7dca97d5f6cb11a5919a5017d722c
Sawan jha <jhasawan02@gmail.com>
2026-02-16 13:45:51 +0530

    setting up mongodb

--- src/app.js
+++ src/app.js
@@ -4,20 +4,20 @@ const app = express();
 
 
 
-app.use("/conatct",(req,res) => {
-    res.send("hello from  the server  from conatct page page!");
+
+
+app.use("/contact",(req,res) => {
+    res.send("hello from  the server  from contact page page!")
 });
 
 app.use("/test",(req,res) => {
     res.send("hello from  the server  from test page!");
 });
-app.use("/conatct",(req,res) => {
-    res.send("hello from  the server  from contact page!");
-});
 
-app.use("/", (req,res) => {
-    res.send("hello from  the server home  page!");
-});
+// app.use("/", (req,res) => {
+//     res.send("hello from  the server home  page!");
+// }); 
+
 
 app.listen(7777, () => {
     console.log(`server is running na dlistening from port : 7777`);
---
+++ src/utilis/constants.js
@@ -0,0 +1,2 @@
+const mongodbUserName = skj138265_db_user;
+const mongodbPassword = A3WHqBi8pSU1gsnU;
